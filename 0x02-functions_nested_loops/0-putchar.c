dea4255a6af967f079994e5b0e6cad89505a65f9
Sherif Awofiranye <awofiranyesherif4@gmail.com>
2023-12-27 00:53:49 +0100

    0x02-functions_nested_loops

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+*main - this program prints _putchar
+*Return: (0) if successful
+*/
+
+int main(void)
+{
+	char var[] = "_putchar";
+	int ii;
+	/*length of the array var*/
+	int lenVar = sizeof(var) / sizeof(var[0]);
+
+	for (ii = 0; ii < lenVar - 1; ii++)
+	{
+		_putchar(var[ii]);
+	}
+	_putchar('\n');
+	return (0);
+}
