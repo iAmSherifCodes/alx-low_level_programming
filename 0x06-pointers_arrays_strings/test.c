6e1ddb969d5eff6a9657534899dd121b28cf8c34
Sherif Awofiranye <awofiranyesherif4@gmail.com>
2024-01-05 16:36:58 +0100

    Coupled commit

---
+++ 0x06-pointers_arrays_strings/test.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(void){
+	    char s1[] = "ABCD";
+    char s2[] = "AB";
+
+    printf("%d\n", strcmp(s1, s2));
+    printf("%d\n", strcmp(s2, s1));
+    printf("%d\n", strcmp(s1, s1));
+    return (0);
+}
+
