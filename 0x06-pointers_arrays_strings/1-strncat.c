f653b3b19a9e35da50ab66c34834c65eb648c38a
Sherif Awofiranye <awofiranyesherif4@gmail.com>
2024-01-05 07:35:28 +0100

    Betty

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,5 +1,15 @@
 #include "main.h"
 
+/**
+ * _strncat - function that concat two strings with byte
+ *
+ * @dest: string to copy to
+ * @src: string to copy from
+ * @n: number of byte
+ *
+ * Return: pointer to the resulting string
+ */
+
 char *_strncat(char *dest, char *src, int n)
 {
 	int dest_len = 0;
