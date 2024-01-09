1402429f5b7e1ef57f5799e0ec1362dfbc51f66d
Sherif Awofiranye <awofiranyesherif4@gmail.com>
2024-01-09 17:14:45 +0100

    Implemented _strchr function

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stddef.h>
+#include <string.h>
+
+/**
+ * _strchr - function that locates a character in a string
+ *
+ * @s: string variable
+ * @c: character to find
+ *
+ * Return: Pointer to first occurence or NULL
+ */
+
+char *_strchr(char *s, char c)
+{
+	int counter;
+	int s_len = (int) strlen(s);
+
+	counter = 0;
+
+	while (counter < s_len)
+	{
+		if (*(s + counter) == c)
+		{
+			return &(*(s + counter));
+		}
+		counter++;
+	}
+
+	return (NULL);
+}
