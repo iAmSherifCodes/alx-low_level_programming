405f0da3183b763f836eb07ac894277062519dad
Sherif Awofiranye <awofiranyesherif4@gmail.com>
2024-01-09 15:53:37 +0100

    Implemented the memset function

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,5 +1,25 @@
 #include "main.h"
 
+/**
+ * _memset - Fills the first n bytes with constant byte b
+ *
+ * @s: copy to
+ * @b: constant byte
+ * @n: n byte
+ *
+ * Return: pointer to s
+ */
+
 char *_memset(char *s, char b, unsigned int n)
 {
+	unsigned int counter;
+
+	counter = 0;
+
+	for ( ; counter < n ; counter++)
+	{
+		*(s + counter) = b;
+	}
+
+	return (s);
 }
