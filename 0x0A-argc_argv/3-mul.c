69c85ced4a5ffa42347d4e7d1f34bfd6532ff242
Sherif Awofiranye <awofiranyesherif4@gmail.com>
2024-01-17 22:33:31 +0100

    Implement args multiplication function

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,26 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
-
 /**
- * main - multiplies two numbers.
- * @argc: argument count
- * @argv: arguments
+ * main - prints the multiplication of two args
  *
- * Return: 0
+ * @c: counts number of args
+ * @argv: array of args
+ *
+ * Return: return int value 0
  */
-int main(int argc, char **argv)
-{
-	int x, y;
 
-	if (argc < 3)
+int main(int c, char **argv)
+{
+	int result;
+
+	result = 0;
+
+	if (c < 2)
 	{
-		printf("Error\n");
-		return (1);
+		puts("Error");
+	}
+	else
+	{
+		result = atoi(*(argv + 1)) * atoi(*(argv + 2));
+		printf("%d\n", result);
 	}
 
-	x = atoi(argv[1]);
-	y = atoi(argv[2]);
-	printf("%d\n", x * y);
-
 	return (0);
+
+
 }
