7c4d66d4eba7db868c09f7093d64d222658e51bf
Sherif Awofiranye <awofiranyesherif4@gmail.com>
2024-01-24 06:37:38 +0100

    02-main.c

--- 0x0D-preprocessor/02-main.c
+++ 0x0D-preprocessor/02-main.c
@@ -1,5 +1,14 @@
 #include <stdio.h>
 
+/**
+ * main - function that prints its name
+ *
+ * @c: args count
+ * @v: args
+ *
+ * Return: returns int value
+ */
+
 int main(int c, char **v)
 {
 	(void)c;
